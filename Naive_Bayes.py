a4bbb998a2900a841ad356ff0b8742ae3429d467
Gina <ginagigo1220.dif06@nctu.edu.tw>
2021-03-29 00:06:06 +0800

    Parse

--- Naive_Bayes.py
+++ Naive_Bayes.py
@@ -8,17 +8,42 @@ import gzip
 import numpy as np
 import matplotlib.pyplot as plt
 
-train_image = gzip.open("train-images-idx3-ubyte.gz", 'r')
+def training_images():
+    with gzip.open("train-images-idx3-ubyte.gz", 'r') as f:
+        """
+            int.from_bytes: bytes -> integer
+            byteorder = 'big' -> big endian
+        """
+        # first 4 bytes = magic number
+        magic_number = int.from_bytes(f.read(4), 'big')
+        # second 4 bytes = number of images
+        num_image = int.from_bytes(f.read(4), 'big')
+        # thrid 4 bytes = row count
+        row_count = int.from_bytes(f.read(4), 'big')
+        # fourth 4 bytes = col count
+        col_count = int.from_bytes(f.read(4), 'big')
+        
+        # rest = image pixel data, each pixel is stored as unsigned byte
+        image_data = f.read()
+        images = np.frombuffer(image_data, dtype=np.uint8).reshape(num_image, row_count, col_count)
+        return images
+    
+def training_label():
+    with gzip.open("train-labels-idx1-ubyte.gz", 'r') as f:
+        # first 4 bytes = magic number
+        magic_number = int.from_bytes(f.read(4), 'big')
+        # second 4 bytes = number of images
+        num_label = int.from_bytes(f.read(4), 'big')
+        
+        # rest = label data, each label is stored as unsigned byte
+        label_data = f.read()
+        labels = np.frombuffer(label_data, dtype=np.uint8).reshape(num_label, 1)
+        return labels
+    
+X_train = training_images()
+y_train = training_label()
 
-image_size = 28
-num_images = 8
-train_image.read(16)
-
-buf = train_image.read(image_size * image_size * num_images)
-data = np.frombuffer(buf, dtype=np.uint8).astype(np.float32)
-data = data.reshape(num_images, image_size, image_size, 1)
-
-# print image
-image = np.asarray(data[0]).squeeze()
+# show image of the first word
+image = np.asarray(X_train[0]).squeeze()
 plt.imshow(image)
 plt.show()
